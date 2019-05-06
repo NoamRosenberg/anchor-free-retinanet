d7b2c729effcb56c933c3805f8a7755e68ea9850
noam <noam@kuandeng.com>
2019-05-06 20:27:57 +0800

    print evaluation

--- coco_eval.py
+++ coco_eval.py
@@ -3,38 +3,27 @@ from __future__ import print_function
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
 
-import numpy as np
 import json
+import pickle
 import os
-
-
-import torch
-import numpy as np
-import torchvision
-import time
-import os
-import copy
-import pdb
-import time
 import argparse
 
 import sys
-#import cv2
+# import cv2
 
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, models, transforms
 
-from dataloader import CocoDataset, CSVDataset, collater, Resizer, AspectRatioBasedSampler, Augmenter, UnNormalizer, Normalizer
-
+from dataloader import CocoDataset, CSVDataset, collater, Resizer, AspectRatioBasedSampler, Augmenter, UnNormalizer, \
+    Normalizer
 
 assert torch.__version__.split('.')[1] == '4'
 
 print('CUDA available: {}'.format(torch.cuda.is_available()))
 
 
-def evaluate_coco(dataset, model, parser = None, threshold=0.05):
-    
+def evaluate_coco(dataset, model, parser=None, threshold=0.05):
     model.eval()
 
     with torch.no_grad():
@@ -47,12 +36,12 @@ def evaluate_coco(dataset, model, parser = None, threshold=0.05):
             data = dataset[index]
             scale = data['scale']
             # run network
-            print(index)
+            #print(index)
             image = data['img'].permute(2, 0, 1).cuda().float().unsqueeze(dim=0)
             scores, labels, boxes = model(image, parser)
             scores = scores.cpu()
             labels = labels.cpu()
-            boxes  = boxes.cpu()
+            boxes = boxes.cpu()
 
             # correct boxes for image scale
             boxes /= scale
@@ -63,7 +52,7 @@ def evaluate_coco(dataset, model, parser = None, threshold=0.05):
                 boxes[:, 3] -= boxes[:, 1]
 
                 # compute predicted labels and scores
-                #for box, score, label in zip(boxes[0], scores[0], labels[0]):
+                # for box, score, label in zip(boxes[0], scores[0], labels[0]):
                 for box_id in range(boxes.shape[0]):
                     score = float(scores[box_id])
                     label = int(labels[box_id])
@@ -75,10 +64,10 @@ def evaluate_coco(dataset, model, parser = None, threshold=0.05):
 
                     # append detection for each positively labeled class
                     image_result = {
-                        'image_id'    : dataset.image_ids[index],
-                        'category_id' : dataset.label_to_coco_label(label),
-                        'score'       : float(score),
-                        'bbox'        : box.tolist(),
+                        'image_id': dataset.image_ids[index],
+                        'category_id': dataset.label_to_coco_label(label),
+                        'score': float(score),
+                        'bbox': box.tolist(),
                     }
 
                     # append detection to results
@@ -109,41 +98,49 @@ def evaluate_coco(dataset, model, parser = None, threshold=0.05):
 
         model.train()
 
-        return
+        return coco_eval
 
 
 def main(args=None):
-	parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
+    parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
 
-	parser.add_argument('--dataset', help='Dataset type, must be one of csv or coco.', default='coco')
-	parser.add_argument('--coco_path', help='Path to COCO directory', default='/data/deeplearning/dataset/coco2017')
-	parser.add_argument('--csv_classes', help='Path to file containing class list (see readme)')
-	parser.add_argument('--csv_val', help='Path to file containing validation annotations (optional, see readme)')
-	parser.add_argument('--s_norm', help='normalize regression outputs', type=float, default=4.0)
-	parser.add_argument('--model', help='Path to model (.pt) file.')
+    parser.add_argument('--dataset', help='Dataset type, must be one of csv or coco.', default='coco')
+    parser.add_argument('--coco_path', help='Path to COCO directory', default='/data/deeplearning/dataset/coco2017')
+    parser.add_argument('--csv_classes', help='Path to file containing class list (see readme)')
+    parser.add_argument('--csv_val', help='Path to file containing validation annotations (optional, see readme)')
+    parser.add_argument('--s_norm', help='normalize regression outputs', type=float, default=4.0)
+    parser.add_argument('--model', help='Path to model (.pt) file.')
 
-	parser = parser.parse_args(args)
+    parser = parser.parse_args(args)
 
-	if parser.dataset == 'coco':
-		dataset_val = CocoDataset(parser.coco_path, set_name='val2017', transform=transforms.Compose([Normalizer(), Resizer()]))
-	elif parser.dataset == 'csv':
-		dataset_val = CSVDataset(train_file=parser.csv_train, class_list=parser.csv_classes, transform=transforms.Compose([Normalizer(), Resizer()]))
-	else:
-		raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
+    if parser.dataset == 'coco':
+        dataset_val = CocoDataset(parser.coco_path, set_name='val2017',
+                                  transform=transforms.Compose([Normalizer(), Resizer()]))
+    elif parser.dataset == 'csv':
+        dataset_val = CSVDataset(train_file=parser.csv_train, class_list=parser.csv_classes,
+                                 transform=transforms.Compose([Normalizer(), Resizer()]))
+    else:
+        raise ValueError('Dataset type not understood (must be csv or coco), exiting.')
 
-	sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
-	dataloader_val = DataLoader(dataset_val, num_workers=1, collate_fn=collater, batch_sampler=sampler_val)
+    sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
+    dataloader_val = DataLoader(dataset_val, num_workers=1, collate_fn=collater, batch_sampler=sampler_val)
 
-	retinanet = torch.load(parser.model)
+    retinanet = torch.load(parser.model)
 
-	use_gpu = True
+    use_gpu = True
 
-	if use_gpu:
-		retinanet = retinanet.cuda()
+    if use_gpu:
+        retinanet = retinanet.cuda()
 
-	retinanet.eval()
+    retinanet.eval()
+    coco_eval = evaluate_coco(dataset_val, retinanet, parser)
+    eval_file = os.path.join('/data/deeplearning/dataset/training/data/newLossRes', 'coco_retinanet_{}.pkl'.format('test'))
 
-    evaluate_coco(dataset_val, retinanet, parser)
+    with open(eval_file, 'wb') as fid:
+        pickle.dump(coco_eval, fid, pickle.HIGHEST_PROTOCOL)
+        print('Wrote COCO eval results to: {}'.format(eval_file))
+
+
 
 if __name__ == '__main__':
- main()
+    main()
