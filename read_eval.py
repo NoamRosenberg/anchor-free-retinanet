56cf335d02a8818a60fd3c4a6f7a90926974da69
noam <noam@kuandeng.com>
2019-05-07 14:48:17 +0800

    evaluate all stages of specific model

--- read_eval.py
+++ read_eval.py
@@ -5,16 +5,17 @@ import os
 def main(args=None):
     parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
 
-    parser.add_argument('--eval_path', help='Path to model (.pt) file.', default='/data/deeplearning/dataset/training/data/new_loss_evaluations')
-
+    parser.add_argument('--eval_path', default='/data/deeplearning/dataset/training/data/new_loss_evaluations')
+    parser.add_argument('--model', default='')
     parser = parser.parse_args(args)
 
     for eval_file in os.listdir(parser.eval_path):
-        with open(os.path.join(parser.eval_path,eval_file),'rb') as f:
-            data = pickle.load(f)
-            print('for {}'.format(eval_file))
-            data.summarize()
-            print('***********************************************')
+        if parser.model in eval_file:
+            with open(os.path.join(parser.eval_path,eval_file),'rb') as f:
+                data = pickle.load(f)
+                print('for {}'.format(eval_file))
+                data.summarize()
+                print('***********************************************')
 
 
 if __name__ == '__main__':
